b149cfcb3c9f9ccc4a926a3452c865ce83c04e6c
Max Kohler <max@koehler-kn.de>
2021-04-24 22:30:24 +0200

    Improve A11y

--- src/initTools.js.js
+++ src/initTools.js.js
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie';
+
 function initTools() {
   const toolbar = document.querySelector(".toolbar");
   const groups = toolbar.querySelectorAll(".options-group")
@@ -7,11 +9,21 @@ function initTools() {
     options.forEach(o => {
       o.addEventListener("change", e => {
         const value = o.getAttribute("value")
-        document.cookie = `${setting}=${value}; samesite=strict;path=/`;
+        Cookies.set(setting, value, { secure: true, sameSite: "strict" });
         document.body.setAttribute(`data-${setting}`, value)
       })
     })
   })
+
+
+  // Set dark/contrast colour theme if user has darkmode enabled and no
+  // other colour theme is set. We don't need to do the same for light mode
+  // because light/contrast is teh default anyway.
+  
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches && Cookies.get("colour") == undefined) {
+    Cookies.set("colour", "dark-contrast", { secure: true, sameSite: "strict" });
+    document.body.setAttribute("data-colour", "dark-contrast")
+  }
 
 }
 export { initTools };
