3d017a4e1988b3d8dffcd5bb5295ee1238c169a2
Max Kohler <max@koehler-kn.de>
2021-05-02 19:57:14 +0200

    Optimise SVG

--- src/markAbbreviations.js
+++ src/markAbbreviations.js
@@ -8,13 +8,14 @@ const dictionary = [
   ["2E", "twice-exceptional"],
   ["WCAG", "Web Content Accessibility Guidelines"],
   ["SMT", "Senior Management Team"],
+  ["BDA", "British Dyslexia Association"],
 ]
 
 function markAbbreviations() {
   const nodes = document.querySelectorAll("p,h1,h2,h3,h4,h5,h6")
   nodes.forEach(node => {
     dictionary.forEach(d => {
-      const re = new RegExp(`${d[0]}`, "g");
+      const re = new RegExp(`\\b${d[0]}\\b`, "g");
       const replacement = d[1];
       node.innerHTML = node.innerHTML.replace(re, `<abbr title="${replacement}">${d[0]}</abbr>`)
     })
